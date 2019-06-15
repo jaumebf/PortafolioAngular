8b4093e559d622a720c4961a064f6b8df6e861b0
Jaume Balaña <jaumewowcore@gmail.com>
2019-06-15 16:54:31 +0200

    configuracion sobre mi

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -6,8 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  public title: string;
+  public subtitle: string;
+  public email: string;
 
-  constructor() { }
+  constructor() { 
+      this.title = "Jaume Balaña";  
+      this.subtitle = "Desarrollador web";
+      this.email = "jaumewowcore@gmail.com";
+  }
 
   ngOnInit() {
   }
